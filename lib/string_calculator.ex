e30d5312566b40fd415696d430afa46ec7db1483
Kerry Buckley <kerryjbuckley@gmail.com>
2022-07-13 19:49:42 +0100

    Extract split_and_sum/2 function

--- lib/string_calculator.ex
+++ lib/string_calculator.ex
@@ -1,11 +1,12 @@
 defmodule StringCalculator do
   def add(""), do: 0
 
-  def add("//" <> <<delimiter::bytes-size(1)>> <> "\n" <> input) do
-    input |> String.split(delimiter) |> Enum.map(&String.to_integer/1) |> Enum.sum()
-  end
+  def add("//" <> <<delimiter::bytes-size(1)>> <> "\n" <> input),
+    do: split_and_sum(input, delimiter)
 
-  def add(input) do
-    input |> String.split(~r/[,\n]/) |> Enum.map(&String.to_integer/1) |> Enum.sum()
+  def add(input), do: split_and_sum(input, ~r/[,\n]/)
+
+  defp split_and_sum(input, split_pattern) do
+    input |> String.split(split_pattern) |> Enum.map(&String.to_integer/1) |> Enum.sum()
   end
 end
